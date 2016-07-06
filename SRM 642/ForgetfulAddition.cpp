b805d7f3bc6ebb526673bff158725de5765ae01b
Nilesh Mishra <nilmish.iit@gmail.com>
2016-07-07 00:40:13 +0530

    finding mini of number give in string

---
+++ SRM 642/ForgetfulAddition.cpp
@@ -0,0 +1,141 @@
+#include <cstdio>
+#include <cmath>
+#include <cstring>
+#include <ctime>
+#include <iostream>
+#include <algorithm>
+#include <set>
+#include <vector>
+#include <sstream>
+#include <typeinfo>
+#include <fstream>
+
+using namespace std;
+
+class ForgetfulAddition {
+    public:
+    int minNumber(string expression) {
+        int n=expression.size();
+        int i,j,digi=0,numb=0;
+        int arr[n],sum[n];
+        
+
+        for(i=0;i<(n-1);++i){
+            digi=(digi*10)+(expression[i]-'0');
+            
+            for(j=i+1;j<n;++j){
+            numb = (numb*10)+(expression[j]-'0');
+            
+           }
+           sum[i]=numb+digi;
+           numb=0;
+           
+        }
+
+
+        int mini=sum[0];
+        for(i=1;i<(n-1);++i){
+            if(mini>sum[i]){
+                mini=sum[i];
+            }
+        }
+        return mini;
+    }
+};
+
+// CUT begin
+ifstream data("ForgetfulAddition.sample");
+
+string next_line() {
+    string s;
+    getline(data, s);
+    return s;
+}
+
+template <typename T> void from_stream(T &t) {
+    stringstream ss(next_line());
+    ss >> t;
+}
+
+void from_stream(string &s) {
+    s = next_line();
+}
+
+template <typename T>
+string to_string(T t) {
+    stringstream s;
+    s << t;
+    return s.str();
+}
+
+string to_string(string t) {
+    return "\"" + t + "\"";
+}
+
+bool do_test(string expression, int __expected) {
+    time_t startClock = clock();
+    ForgetfulAddition *instance = new ForgetfulAddition();
+    int __result = instance->minNumber(expression);
+    double elapsed = (double)(clock() - startClock) / CLOCKS_PER_SEC;
+    delete instance;
+
+    if (__result == __expected) {
+        cout << "PASSED!" << " (" << elapsed << " seconds)" << endl;
+        return true;
+    }
+    else {
+        cout << "FAILED!" << " (" << elapsed << " seconds)" << endl;
+        cout << "           Expected: " << to_string(__expected) << endl;
+        cout << "           Received: " << to_string(__result) << endl;
+        return false;
+    }
+}
+
+int run_test(bool mainProcess, const set<int> &case_set, const string command) {
+    int cases = 0, passed = 0;
+    while (true) {
+        if (next_line().find("--") != 0)
+            break;
+        string expression;
+        from_stream(expression);
+        next_line();
+        int __answer;
+        from_stream(__answer);
+
+        cases++;
+        if (case_set.size() > 0 && case_set.find(cases - 1) == case_set.end())
+            continue;
+
+        cout << "  Testcase #" << cases - 1 << " ... ";
+        if ( do_test(expression, __answer)) {
+            passed++;
+        }
+    }
+    if (mainProcess) {
+        cout << endl << "Passed : " << passed << "/" << cases << " cases" << endl;
+        int T = time(NULL) - 1467629911;
+        double PT = T / 60.0, TT = 75.0;
+        cout << "Time   : " << T / 60 << " minutes " << T % 60 << " secs" << endl;
+        cout << "Score  : " << 250 * (0.3 + (0.7 * TT * TT) / (10.0 * PT * PT + TT * TT)) << " points" << endl;
+    }
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+    cout.setf(ios::fixed, ios::floatfield);
+    cout.precision(2);
+    set<int> cases;
+    bool mainProcess = true;
+    for (int i = 1; i < argc; ++i) {
+        if ( string(argv[i]) == "-") {
+            mainProcess = false;
+        } else {
+            cases.insert(atoi(argv[i]));
+        }
+    }
+    if (mainProcess) {
+        cout << "ForgetfulAddition (250 Points)" << endl << endl;
+    }
+    return run_test(mainProcess, cases, argv[0]);
+}
+// CUT end
