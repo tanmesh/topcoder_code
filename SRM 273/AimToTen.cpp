24a017959104394154a4b93a330cddb67c244e6a
Nilesh Mishra <nilmish.iit@gmail.com>
2016-07-22 21:20:27 +0530

    avg_grade should be 9.5 or above

---
+++ SRM 273/AimToTen.cpp
@@ -0,0 +1,144 @@
+#include <cstdio>
+#include <cmath>
+#include <cstring>
+#include <ctime>
+#include <iostream>
+#include <algorithm>
+#include <set>
+#include <vector>
+#include <sstream>
+#include <typeinfo>
+#include <fstream>
+
+using namespace std;
+
+class AimToTen {
+    public:
+    int need(vector<int> marks) {
+        double sum=0,avg_grd=0;
+        for(int i=0;i<marks.size();++i){
+            sum+=marks[i];
+            //cout<<sum<<endl;
+        }
+        avg_grd=sum/marks.size();
+        if(avg_grd>=9.5){
+            return 0;
+        }
+        int count=0;
+        while(avg_grd<9.5){
+            sum+=10;
+            ++count;
+            avg_grd=sum/(marks.size()+count);
+            //cout<<sum<<" "<<avg_grd<<" "<<count<<endl;
+        }
+        return count;
+    }
+};
+
+// CUT begin
+ifstream data("AimToTen.sample");
+
+string next_line() {
+    string s;
+    getline(data, s);
+    return s;
+}
+
+template <typename T> void from_stream(T &t) {
+    stringstream ss(next_line());
+    ss >> t;
+}
+
+void from_stream(string &s) {
+    s = next_line();
+}
+
+template <typename T> void from_stream(vector<T> &ts) {
+    int len;
+    from_stream(len);
+    ts.clear();
+    for (int i = 0; i < len; ++i) {
+        T t;
+        from_stream(t);
+        ts.push_back(t);
+    }
+}
+
+template <typename T>
+string to_string(T t) {
+    stringstream s;
+    s << t;
+    return s.str();
+}
+
+string to_string(string t) {
+    return "\"" + t + "\"";
+}
+
+bool do_test(vector<int> marks, int __expected) {
+    time_t startClock = clock();
+    AimToTen *instance = new AimToTen();
+    int __result = instance->need(marks);
+    double elapsed = (double)(clock() - startClock) / CLOCKS_PER_SEC;
+    delete instance;
+
+    if (__result == __expected) {
+        cout << "PASSED!" << " (" << elapsed << " seconds)" << endl;
+        return true;
+    }
+    else {
+        cout << "FAILED!" << " (" << elapsed << " seconds)" << endl;
+        cout << "           Expected: " << to_string(__expected) << endl;
+        cout << "           Received: " << to_string(__result) << endl;
+        return false;
+    }
+}
+
+int run_test(bool mainProcess, const set<int> &case_set, const string command) {
+    int cases = 0, passed = 0;
+    while (true) {
+        if (next_line().find("--") != 0)
+            break;
+        vector<int> marks;
+        from_stream(marks);
+        next_line();
+        int __answer;
+        from_stream(__answer);
+
+        cases++;
+        if (case_set.size() > 0 && case_set.find(cases - 1) == case_set.end())
+            continue;
+
+        cout << "  Testcase #" << cases - 1 << " ... ";
+        if ( do_test(marks, __answer)) {
+            passed++;
+        }
+    }
+    if (mainProcess) {
+        cout << endl << "Passed : " << passed << "/" << cases << " cases" << endl;
+        int T = time(NULL) - 1469196880;
+        double PT = T / 60.0, TT = 75.0;
+        cout << "Time   : " << T / 60 << " minutes " << T % 60 << " secs" << endl;
+        cout << "Score  : " << 250 * (0.3 + (0.7 * TT * TT) / (10.0 * PT * PT + TT * TT)) << " points" << endl;
+    }
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+    cout.setf(ios::fixed, ios::floatfield);
+    cout.precision(2);
+    set<int> cases;
+    bool mainProcess = true;
+    for (int i = 1; i < argc; ++i) {
+        if ( string(argv[i]) == "-") {
+            mainProcess = false;
+        } else {
+            cases.insert(atoi(argv[i]));
+        }
+    }
+    if (mainProcess) {
+        cout << "AimToTen (250 Points)" << endl << endl;
+    }
+    return run_test(mainProcess, cases, argv[0]);
+}
+// CUT end
