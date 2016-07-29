e5b860a38e435b90bafe51e657548d997cae72ef
Nilesh Mishra <nilmish.iit@gmail.com>
2016-07-29 12:26:52 +0530

    Esperan to Numbers

---
+++ SRM 312/EsperantoNumbers.cpp
@@ -0,0 +1,145 @@
+#include <cstdio>
+#include <cmath>
+#include <cstring>
+#include <ctime>
+#include <iostream>
+#include <algorithm>
+#include <set>
+#include <vector>
+#include <sstream>
+#include <typeinfo>
+#include <fstream>
+
+using namespace std;
+
+class EsperantoNumbers {
+    public:
+    string nameThisNumber(int x) {
+        int n=x,count=0;
+        string st="";    
+        string arr[11]={"","unu","du","tri","kvar","kvin","ses","sep","ok","nau","dek"};
+        // if(n<11){
+        //     st=st+arr[n];
+        // }
+        // n=0;
+        while(n>0){
+            int last=n%10;
+            ++count;
+            if(n==10){
+                st=st+arr[n];
+                break;
+            }
+            if(count==1){
+                st=st+arr[last];
+            }
+            if(last!=1 && x%10!=0 && count==2){
+                st=arr[last]+"dek "+st;
+            }
+            if(last==1 && x%10!=0 && count==2){
+                st="dek "+st;
+            }
+            if(x%10==0 && count==2){
+                st=arr[last]+"dek"+st;
+            }
+            n=n/10;
+        }
+        return st;
+    }
+};
+
+// CUT begin
+ifstream data("EsperantoNumbers.sample");
+
+string next_line() {
+    string s;
+    getline(data, s);
+    return s;
+}
+
+template <typename T> void from_stream(T &t) {
+    stringstream ss(next_line());
+    ss >> t;
+}
+
+void from_stream(string &s) {
+    s = next_line();
+}
+
+template <typename T>
+string to_string(T t) {
+    stringstream s;
+    s << t;
+    return s.str();
+}
+
+string to_string(string t) {
+    return "\"" + t + "\"";
+}
+
+bool do_test(int x, string __expected) {
+    time_t startClock = clock();
+    EsperantoNumbers *instance = new EsperantoNumbers();
+    string __result = instance->nameThisNumber(x);
+    double elapsed = (double)(clock() - startClock) / CLOCKS_PER_SEC;
+    delete instance;
+
+    if (__result == __expected) {
+        cout << "PASSED!" << " (" << elapsed << " seconds)" << endl;
+        return true;
+    }
+    else {
+        cout << "FAILED!" << " (" << elapsed << " seconds)" << endl;
+        cout << "           Expected: " << to_string(__expected) << endl;
+        cout << "           Received: " << to_string(__result) << endl;
+        return false;
+    }
+}
+
+int run_test(bool mainProcess, const set<int> &case_set, const string command) {
+    int cases = 0, passed = 0;
+    while (true) {
+        if (next_line().find("--") != 0)
+            break;
+        int x;
+        from_stream(x);
+        next_line();
+        string __answer;
+        from_stream(__answer);
+
+        cases++;
+        if (case_set.size() > 0 && case_set.find(cases - 1) == case_set.end())
+            continue;
+
+        cout << "  Testcase #" << cases - 1 << " ... ";
+        if ( do_test(x, __answer)) {
+            passed++;
+        }
+    }
+    if (mainProcess) {
+        cout << endl << "Passed : " << passed << "/" << cases << " cases" << endl;
+        int T = time(NULL) - 1469727050;
+        double PT = T / 60.0, TT = 75.0;
+        cout << "Time   : " << T / 60 << " minutes " << T % 60 << " secs" << endl;
+        cout << "Score  : " << 250 * (0.3 + (0.7 * TT * TT) / (10.0 * PT * PT + TT * TT)) << " points" << endl;
+    }
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+    cout.setf(ios::fixed, ios::floatfield);
+    cout.precision(2);
+    set<int> cases;
+    bool mainProcess = true;
+    for (int i = 1; i < argc; ++i) {
+        if ( string(argv[i]) == "-") {
+            mainProcess = false;
+        } else {
+            cases.insert(atoi(argv[i]));
+        }
+    }
+    if (mainProcess) {
+        cout << "EsperantoNumbers (250 Points)" << endl << endl;
+    }
+    return run_test(mainProcess, cases, argv[0]);
+}
+// CUT end
