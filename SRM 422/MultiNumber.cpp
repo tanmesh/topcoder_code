29c257d859ab46839011b8e9df6f555d00d42214
Tanmesh Mishra <tanmeshnm@gmail.com>
2017-01-18 23:23:37 +0530

    MultiNumber 18Jan17

---
+++ SRM 422/MultiNumber.cpp
@@ -0,0 +1,145 @@
+#include <cstdio>
+#include <cmath>
+#include <cstring>
+#include <ctime>
+#include <iostream>
+#include <algorithm>
+#include <set>
+#include <vector>
+#include <sstream>
+#include <typeinfo>
+#include <fstream>
+
+using namespace std;
+
+class MultiNumber {
+    public:
+    int give_length(int n){
+        int cnt=0;
+        while(n>0){
+            n/=10;
+            ++cnt;
+        }
+        return cnt;
+    }
+    string check(int number) {
+        int n=give_length(number);
+        if(n==1){
+            return "NO";
+        }
+        int a[n];
+        for(int i=0;i<n;++i){
+            a[n-1-i]=number%10;
+            number/=10;
+        }
+        int n1=1;
+        for(int i=0;i<n;++i){
+            n1=n1*a[i];
+            int n2=1;
+            for(int j=i+1;j<n;++j){
+                n2=n2*a[j];
+            }
+            if(n1 == n2){
+                return "YES";
+            }
+        }
+        return "NO";
+    }
+};
+
+// CUT begin
+ifstream data("MultiNumber.sample");
+
+string next_line() {
+    string s;
+    getline(data, s);
+    return s;
+}
+
+template <typename T> void from_stream(T &t) {
+    stringstream ss(next_line());
+    ss >> t;
+}
+
+void from_stream(string &s) {
+    s = next_line();
+}
+
+template <typename T>
+string to_string(T t) {
+    stringstream s;
+    s << t;
+    return s.str();
+}
+
+string to_string(string t) {
+    return "\"" + t + "\"";
+}
+
+bool do_test(int number, string __expected) {
+    time_t startClock = clock();
+    MultiNumber *instance = new MultiNumber();
+    string __result = instance->check(number);
+    double elapsed = (double)(clock() - startClock) / CLOCKS_PER_SEC;
+    delete instance;
+
+    if (__result == __expected) {
+        cout << "PASSED!" << " (" << elapsed << " seconds)" << endl;
+        return true;
+    }
+    else {
+        cout << "FAILED!" << " (" << elapsed << " seconds)" << endl;
+        cout << "           Expected: " << to_string(__expected) << endl;
+        cout << "           Received: " << to_string(__result) << endl;
+        return false;
+    }
+}
+
+int run_test(bool mainProcess, const set<int> &case_set, const string command) {
+    int cases = 0, passed = 0;
+    while (true) {
+        if (next_line().find("--") != 0)
+            break;
+        int number;
+        from_stream(number);
+        next_line();
+        string __answer;
+        from_stream(__answer);
+
+        cases++;
+        if (case_set.size() > 0 && case_set.find(cases - 1) == case_set.end())
+            continue;
+
+        cout << "  Testcase #" << cases - 1 << " ... ";
+        if ( do_test(number, __answer)) {
+            passed++;
+        }
+    }
+    if (mainProcess) {
+        cout << endl << "Passed : " << passed << "/" << cases << " cases" << endl;
+        int T = time(NULL) - 1484761279;
+        double PT = T / 60.0, TT = 75.0;
+        cout << "Time   : " << T / 60 << " minutes " << T % 60 << " secs" << endl;
+        cout << "Score  : " << 250 * (0.3 + (0.7 * TT * TT) / (10.0 * PT * PT + TT * TT)) << " points" << endl;
+    }
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+    cout.setf(ios::fixed, ios::floatfield);
+    cout.precision(2);
+    set<int> cases;
+    bool mainProcess = true;
+    for (int i = 1; i < argc; ++i) {
+        if ( string(argv[i]) == "-") {
+            mainProcess = false;
+        } else {
+            cases.insert(atoi(argv[i]));
+        }
+    }
+    if (mainProcess) {
+        cout << "MultiNumber (250 Points)" << endl << endl;
+    }
+    return run_test(mainProcess, cases, argv[0]);
+}
+// CUT end
