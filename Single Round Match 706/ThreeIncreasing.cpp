44abddc3c5e14cc931a97fc16f6febe1b2a0e50c
Tanmesh Mishra <tanmeshnm@gmail.com>
2017-01-21 23:30:52 +0530

    ThreeIncreasing 21Jan17

---
+++ Single Round Match 706/ThreeIncreasing.cpp
@@ -0,0 +1,142 @@
+#include <cstdio>
+#include <cmath>
+#include <cstring>
+#include <ctime>
+#include <iostream>
+#include <algorithm>
+#include <set>
+#include <vector>
+#include <sstream>
+#include <typeinfo>
+#include <fstream>
+
+using namespace std;
+
+class ThreeIncreasing {
+    public:
+    int minEaten(int a, int b, int c) {
+        int eat=0;
+        if(b>=c){
+            int tmp=b-c+1;
+            b=b-tmp;
+            if(b>0){
+                eat+=tmp;
+            }
+            else{
+                return -1;
+            }
+        }
+        if(a>=b){
+            int tmp=a-b+1;
+            a=a-tmp;
+            if(a>0){
+                eat+=tmp;
+            }
+            else{
+                return -1;
+            }
+        }
+        return eat;
+    }
+};
+
+// CUT begin
+ifstream data("ThreeIncreasing.sample");
+
+string next_line() {
+    string s;
+    getline(data, s);
+    return s;
+}
+
+template <typename T> void from_stream(T &t) {
+    stringstream ss(next_line());
+    ss >> t;
+}
+
+void from_stream(string &s) {
+    s = next_line();
+}
+
+template <typename T>
+string to_string(T t) {
+    stringstream s;
+    s << t;
+    return s.str();
+}
+
+string to_string(string t) {
+    return "\"" + t + "\"";
+}
+
+bool do_test(int a, int b, int c, int __expected) {
+    time_t startClock = clock();
+    ThreeIncreasing *instance = new ThreeIncreasing();
+    int __result = instance->minEaten(a, b, c);
+    double elapsed = (double)(clock() - startClock) / CLOCKS_PER_SEC;
+    delete instance;
+
+    if (__result == __expected) {
+        cout << "PASSED!" << " (" << elapsed << " seconds)" << endl;
+        return true;
+    }
+    else {
+        cout << "FAILED!" << " (" << elapsed << " seconds)" << endl;
+        cout << "           Expected: " << to_string(__expected) << endl;
+        cout << "           Received: " << to_string(__result) << endl;
+        return false;
+    }
+}
+
+int run_test(bool mainProcess, const set<int> &case_set, const string command) {
+    int cases = 0, passed = 0;
+    while (true) {
+        if (next_line().find("--") != 0)
+            break;
+        int a;
+        from_stream(a);
+        int b;
+        from_stream(b);
+        int c;
+        from_stream(c);
+        next_line();
+        int __answer;
+        from_stream(__answer);
+
+        cases++;
+        if (case_set.size() > 0 && case_set.find(cases - 1) == case_set.end())
+            continue;
+
+        cout << "  Testcase #" << cases - 1 << " ... ";
+        if ( do_test(a, b, c, __answer)) {
+            passed++;
+        }
+    }
+    if (mainProcess) {
+        cout << endl << "Passed : " << passed << "/" << cases << " cases" << endl;
+        int T = time(NULL) - 1485014410;
+        double PT = T / 60.0, TT = 75.0;
+        cout << "Time   : " << T / 60 << " minutes " << T % 60 << " secs" << endl;
+        cout << "Score  : " << 250 * (0.3 + (0.7 * TT * TT) / (10.0 * PT * PT + TT * TT)) << " points" << endl;
+    }
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+    cout.setf(ios::fixed, ios::floatfield);
+    cout.precision(2);
+    set<int> cases;
+    bool mainProcess = true;
+    for (int i = 1; i < argc; ++i) {
+        if ( string(argv[i]) == "-") {
+            mainProcess = false;
+        } else {
+            cases.insert(atoi(argv[i]));
+        }
+    }
+    if (mainProcess) {
+        cout << "ThreeIncreasing (250 Points)" << endl << endl;
+    }
+    return run_test(mainProcess, cases, argv[0]);
+}
+// CUT end
