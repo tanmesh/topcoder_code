65246a160e25c39d09eaa7c7b4db44c40f718319
Tanmesh <tanmesh@iTanmesh.local>
2017-03-01 21:14:05 +0530

    LeastMajorityMultiple 1Mar17

---
+++ SRM 434/LeastMajorityMultiple.cpp
@@ -0,0 +1,139 @@
+#include <cstdio>
+#include <cmath>
+#include <cstring>
+#include <ctime>
+#include <iostream>
+#include <algorithm>
+#include <set>
+#include <vector>
+#include <sstream>
+#include <typeinfo>
+#include <fstream>
+
+using namespace std;
+
+class LeastMajorityMultiple {
+    public:
+    int leastMajorityMultiple(int a, int b, int c, int d, int e) {
+        vector<int> arr;
+        arr.push_back(a);
+        arr.push_back(b);
+        arr.push_back(c);
+        arr.push_back(d);
+        arr.push_back(e);
+        sort(arr.begin(),arr.end());
+        for(int i=arr[0];;++i){
+            int cnt=0;
+            for(int j=0;j<5;++j){
+                if(i%arr[j] == 0) ++cnt;
+            }
+            if(cnt >= 3) return i;
+        }
+        return 0;
+    }
+};
+
+// CUT begin
+ifstream data("LeastMajorityMultiple.sample");
+
+string next_line() {
+    string s;
+    getline(data, s);
+    return s;
+}
+
+template <typename T> void from_stream(T &t) {
+    stringstream ss(next_line());
+    ss >> t;
+}
+
+void from_stream(string &s) {
+    s = next_line();
+}
+
+template <typename T>
+string to_string(T t) {
+    stringstream s;
+    s << t;
+    return s.str();
+}
+
+string to_string(string t) {
+    return "\"" + t + "\"";
+}
+
+bool do_test(int a, int b, int c, int d, int e, int __expected) {
+    time_t startClock = clock();
+    LeastMajorityMultiple *instance = new LeastMajorityMultiple();
+    int __result = instance->leastMajorityMultiple(a, b, c, d, e);
+    double elapsed = (double)(clock() - startClock) / CLOCKS_PER_SEC;
+    delete instance;
+
+    if (__result == __expected) {
+        cout << "PASSED!" << " (" << elapsed << " seconds)" << endl;
+        return true;
+    }
+    else {
+        cout << "FAILED!" << " (" << elapsed << " seconds)" << endl;
+        cout << "           Expected: " << to_string(__expected) << endl;
+        cout << "           Received: " << to_string(__result) << endl;
+        return false;
+    }
+}
+
+int run_test(bool mainProcess, const set<int> &case_set, const string command) {
+    int cases = 0, passed = 0;
+    while (true) {
+        if (next_line().find("--") != 0)
+            break;
+        int a;
+        from_stream(a);
+        int b;
+        from_stream(b);
+        int c;
+        from_stream(c);
+        int d;
+        from_stream(d);
+        int e;
+        from_stream(e);
+        next_line();
+        int __answer;
+        from_stream(__answer);
+
+        cases++;
+        if (case_set.size() > 0 && case_set.find(cases - 1) == case_set.end())
+            continue;
+
+        cout << "  Testcase #" << cases - 1 << " ... ";
+        if ( do_test(a, b, c, d, e, __answer)) {
+            passed++;
+        }
+    }
+    if (mainProcess) {
+        cout << endl << "Passed : " << passed << "/" << cases << " cases" << endl;
+        int T = time(NULL) - 1488382277;
+        double PT = T / 60.0, TT = 75.0;
+        cout << "Time   : " << T / 60 << " minutes " << T % 60 << " secs" << endl;
+        cout << "Score  : " << 250 * (0.3 + (0.7 * TT * TT) / (10.0 * PT * PT + TT * TT)) << " points" << endl;
+    }
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+    cout.setf(ios::fixed, ios::floatfield);
+    cout.precision(2);
+    set<int> cases;
+    bool mainProcess = true;
+    for (int i = 1; i < argc; ++i) {
+        if ( string(argv[i]) == "-") {
+            mainProcess = false;
+        } else {
+            cases.insert(atoi(argv[i]));
+        }
+    }
+    if (mainProcess) {
+        cout << "LeastMajorityMultiple (250 Points)" << endl << endl;
+    }
+    return run_test(mainProcess, cases, argv[0]);
+}
+// CUT end
