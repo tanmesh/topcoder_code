9d0f43469746b4f8e76a318f8a2a142fcf9d3463
tanmesh <tanmeshnm@gmail.com>
2017-04-14 13:36:43 +0530

    FourBlocksEasy 14Apr17

---
+++ SRM 444/FourBlocksEasy.cpp
@@ -0,0 +1,150 @@
+#include <cstdio>
+#include <cmath>
+#include <cstring>
+#include <ctime>
+#include <iostream>
+#include <algorithm>
+#include <set>
+#include <vector>
+#include <sstream>
+#include <typeinfo>
+#include <fstream>
+
+using namespace std;
+
+class FourBlocksEasy {
+    public:
+    int maxScore(vector<string> grid) {
+        int total_dot=0;
+        for(int i=0;i<grid.size();++i){
+            for(int j=0;j<grid[i].length();++j){
+                if(grid[i][j] == '.') ++total_dot;
+            }
+        }
+        int cnt=0;
+        for(int i=0;i+1<grid.size();++i){
+            for(int j=0;j+1<grid[i].length();++j){
+                if(grid[i][j] == '.'){
+                    if(grid[i][j+1] == '.'){
+                        if(grid[i+1][j] == '.'){
+                            if(grid[i+1][j+1] == '.'){
+                                cnt++;
+                                ++j;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        int four_scr=cnt*16 , one_scr=grid.size()*grid[0].length()-cnt*4;
+        return four_scr+one_scr;
+    }
+};
+
+// CUT begin
+ifstream data("FourBlocksEasy.sample");
+
+string next_line() {
+    string s;
+    getline(data, s);
+    return s;
+}
+
+template <typename T> void from_stream(T &t) {
+    stringstream ss(next_line());
+    ss >> t;
+}
+
+void from_stream(string &s) {
+    s = next_line();
+}
+
+template <typename T> void from_stream(vector<T> &ts) {
+    int len;
+    from_stream(len);
+    ts.clear();
+    for (int i = 0; i < len; ++i) {
+        T t;
+        from_stream(t);
+        ts.push_back(t);
+    }
+}
+
+template <typename T>
+string to_string(T t) {
+    stringstream s;
+    s << t;
+    return s.str();
+}
+
+string to_string(string t) {
+    return "\"" + t + "\"";
+}
+
+bool do_test(vector<string> grid, int __expected) {
+    time_t startClock = clock();
+    FourBlocksEasy *instance = new FourBlocksEasy();
+    int __result = instance->maxScore(grid);
+    double elapsed = (double)(clock() - startClock) / CLOCKS_PER_SEC;
+    delete instance;
+
+    if (__result == __expected) {
+        cout << "PASSED!" << " (" << elapsed << " seconds)" << endl;
+        return true;
+    }
+    else {
+        cout << "FAILED!" << " (" << elapsed << " seconds)" << endl;
+        cout << "           Expected: " << to_string(__expected) << endl;
+        cout << "           Received: " << to_string(__result) << endl;
+        return false;
+    }
+}
+
+int run_test(bool mainProcess, const set<int> &case_set, const string command) {
+    int cases = 0, passed = 0;
+    while (true) {
+        if (next_line().find("--") != 0)
+            break;
+        vector<string> grid;
+        from_stream(grid);
+        next_line();
+        int __answer;
+        from_stream(__answer);
+
+        cases++;
+        if (case_set.size() > 0 && case_set.find(cases - 1) == case_set.end())
+            continue;
+
+        cout << "  Testcase #" << cases - 1 << " ... ";
+        if ( do_test(grid, __answer)) {
+            passed++;
+        }
+    }
+    if (mainProcess) {
+        cout << endl << "Passed : " << passed << "/" << cases << " cases" << endl;
+        int T = time(NULL) - 1488469044;
+        double PT = T / 60.0, TT = 75.0;
+        cout << "Time   : " << T / 60 << " minutes " << T % 60 << " secs" << endl;
+        cout << "Score  : " << 250 * (0.3 + (0.7 * TT * TT) / (10.0 * PT * PT + TT * TT)) << " points" << endl;
+    }
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+    cout.setf(ios::fixed, ios::floatfield);
+    cout.precision(2);
+    set<int> cases;
+    bool mainProcess = true;
+    for (int i = 1; i < argc; ++i) {
+        if ( string(argv[i]) == "-") {
+            mainProcess = false;
+        } else {
+            cases.insert(atoi(argv[i]));
+        }
+    }
+    if (mainProcess) {
+        cout << "FourBlocksEasy (250 Points)" << endl << endl;
+    }
+    return run_test(mainProcess, cases, argv[0]);
+}
+// CUT end
